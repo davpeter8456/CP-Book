02401999b1ce7495abd9ec989a4c9dc0e6c816bd
DavydovPeter <davpeter8456@gmail.com>
2025-03-23 12:51:55 +0300

    default

---
+++ code_template.cpp
@@ -0,0 +1,17 @@
+// UwU
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+
+typedef long long ll;
+typedef long double ld;
+
+inline void solve() {}
+
+signed main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll tt = 1;
+    //std::cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
